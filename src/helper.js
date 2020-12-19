6c8b525f49e886d21eb5bd5d935533e27c8416da
axios <axiosleo@foxmail.com>
2020-12-19 22:06:37 +0800

    feat: add helper methods

--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+const promisify = require('util').promisify;
+const exists = promisify(fs.exists);
+const mkdir = promisify(fs.mkdir);
+const writeFile = promisify(fs.writeFile);
+
 const { prompt, Select } = require('enquirer');
 
 async function ask(message = '') {
@@ -34,8 +41,26 @@ async function select(choices = [], message = '') {
   return await prompt.run();
 }
 
+function _upperFirst(str) {
+  if (!str) {
+    return '';
+  }
+  return str[0].toUpperCase() + str.substring(1);
+}
+
+async function _write(filepath, content) {
+  const dir = path.dirname(filepath);
+  let exist = await exists(dir);
+  if (!exist) {
+    await mkdir(dir, { recursive: true });
+  }
+  await writeFile(filepath, content);
+}
+
 module.exports = {
   ask,
   select,
-  confirm
+  confirm,
+  _write,
+  _upperFirst
 };
