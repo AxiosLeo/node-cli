a9d6df5ab18100c8381d4456964523deb6245cc5
axios <axiosleo@foxmail.com>
2021-01-31 01:15:10 +0800

    feat: add file system helper methods

--- src/snippets/fs.js
+++ src/snippets/fs.js
@@ -6,6 +6,18 @@ const promisify = require('util').promisify;
 const exists = promisify(fs.exists);
 const mkdir = promisify(fs.mkdir);
 const writeFile = promisify(fs.writeFile);
+const readFile = promisify(fs.readFile);
+const copyFile = promisify(fs.copyFile);
+const rename = promisify(fs.rename);
+const stat = promisify(fs.stat);
+const readdir = promisify(fs.readdir);
+const unlink = promisify(fs.unlink);
+const rmdir = promisify(fs.rmdir);
+
+function _ext(filename) {
+  let tmp = path.extname(filename || '').split('.');
+  return tmp.length > 1 ? tmp.pop() : '';
+}
 
 async function _write(filepath, content) {
   const dir = path.dirname(filepath);
@@ -16,6 +28,102 @@ async function _write(filepath, content) {
   await writeFile(filepath, content);
 }
 
+async function _append(filepath, content) {
+  await writeFile(filepath, content, { flag: 'a' });
+}
+
+async function _read(filepath) {
+  return await readFile(filepath, 'utf-8');
+}
+
+async function _read_json(filepath) {
+  const content = await readFile(path.resolve(filepath), 'utf-8');
+  return JSON.parse(content);
+}
+
+async function _mkdir(dir) {
+  const exist = await exists(dir);
+  if (!exist) {
+    await mkdir(dir, { recursive: true });
+  }
+}
+
+async function _exists(filepath) {
+  return await exists(filepath);
+}
+
+async function _move(source, target) {
+  if (await _exists(source)) {
+    await _mkdir(path.dirname(target));
+    await rename(source, target);
+  }
+}
+
+async function _copy(source, target) {
+  if (await _exists(source)) {
+    await _mkdir(path.dirname(target));
+    await copyFile(source, target);
+  }
+}
+
+async function _is_file(filepath) {
+  const status = await stat(filepath);
+  return status.isFile();
+}
+
+async function _is_dir(dirpath) {
+  const status = await stat(dirpath);
+  return status.isDirectory();
+}
+
+async function _search(dir, ext = '*', recur = true) {
+  if (!(await _is_dir(dir))) {
+    throw new Error('Only support dir path');
+  }
+  let files = [];
+  const exts = ext && ext !== '*' ? ext.split('|') : [];
+  const tmp = await readdir(dir);
+  await Promise.all(tmp.map(async (filename) => {
+    const full = path.join(dir, filename);
+    if (await _is_file(full)) {
+      const file_ext = _ext(filename);
+      if (!exts.length || exts.indexOf(file_ext) > -1) {
+        files.push(full);
+      }
+    } else if (await _is_dir(full) && recur) {
+      (await _search(full)).forEach(item => files.push(item));
+    }
+  }));
+  return files;
+}
+
+async function _remove(filepath) {
+  if (filepath === path.sep) {
+    console.error(`cannot delete root of system with : ${filepath}`);
+    return;
+  }
+  {if (await _exists(filepath)) {
+    if (await _is_file(filepath)) {
+      await unlink(filepath);
+    } else if(await _is_dir(filepath)){
+      await rmdir(filepath);
+    }
+  }}
+}
+
+
 module.exports = {
-  _write
+  _ext,
+  _read,
+  _copy,
+  _move,
+  _mkdir,
+  _write,
+  _remove,
+  _search,
+  _exists,
+  _append,
+  _is_dir,
+  _is_file,
+  _read_json
 };
