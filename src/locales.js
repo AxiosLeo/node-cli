37212e9d825b14ba5276b66fd29f77c500c4d2be
axios <axiosleo@foxmail.com>
2021-02-06 23:01:09 +0800

    fix: locales use method

--- src/locales.js
+++ src/locales.js
@@ -5,7 +5,7 @@ const osLocale = require('os-locale');
 const { I18n } = require('i18n');
 const { _render } = require('./helper/str');
 
-let i18n, sets;
+let i18n, sets, options;
 
 function __(str, params = null) {
   if (!params) {
@@ -26,18 +26,20 @@ function init(config) {
   if (sets.indexOf(locale) < 0) {
     locale = default_locale;
   }
-  i18n = new I18n({
+  options = {
     defaultLocale: locale,
     locales: sets,         // The supported locales, expects an array of locale strings
     directory: config.dir, // The path to the language packs directory
-  });
+  };
+  i18n = new I18n(options);
 }
 
 function use(set) {
   if (sets.indexOf(set) < 0) {
     debug.error(`${set} not exist in config.sets which is (${sets.join(',')}) `);
   }
-  i18n.defaultLocale = set;
+  options.defaultLocale = set;
+  i18n = new I18n(options);
 }
 
 module.exports = {
