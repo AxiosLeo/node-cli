8601bc7e2e525e95c7da8379418f5ac9ed68933f
axios <axiosleo@foxmail.com>
2021-02-06 23:02:54 +0800

    test: add locales test case

---
+++ nyc.config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  exclude: [
+    'coverage',
+    'self-coverage',
+    'tests/*.js',
+    'bin',
+    'locales',
+    'commands'
+  ],
+  branches: 100,
+  functions: 100,
+  lines: 100,
+  statements: 100
+};
