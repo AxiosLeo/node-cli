6ff1f02d576008dc46652cc6404b9e588cbe6d23
axios <axiosleo@foxmail.com>
2021-02-07 21:09:59 +0800

    chore: patch commands/README.md file content

--- commands/init.js
+++ commands/init.js
@@ -80,7 +80,7 @@ app.start({
     _write(path.join(output, `bin/${name}.js`), content);
 
     await _write(path.join(output, 'commands/README.md'), `you can write code of commands in here.
-or you can use \`cli-tool make <command-name> <command-dir-path>\` to make a command code in here.
+or you can use \`cli-tool make <command-name> <command-dir-path>\` to make a command file in here.
 
 command example: 
 cli-tool make test ./commands/
