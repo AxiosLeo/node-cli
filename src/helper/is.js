428de525bf01231ccb4b3436467b243183229cdc
axios <axiosleo@foxmail.com>
2021-02-10 16:03:58 +0800

    feat: improve is helper

--- src/helper/is.js
+++ src/helper/is.js
@@ -7,15 +7,33 @@ const is = {
   array: a => Array.isArray(a),
   string: a => typeof (a) === 'string',
   number: a => typeof (a) === 'number',
-  object: a => typeof (a) === 'object',
+  object: a => a !== null && typeof a === 'object' && Array.isArray(a) === false,
   function: a => typeof (a) === 'function',
   boolean: a => typeof (a) === 'boolean',
-  bigint: a => typeof (a) === 'bigint',
-  file: async a => fs._is_file(a),
-  dir: async a => fs._is_dir(a),
+  file: async a => await fs._is_file(a),
+  dir: async a => await fs._is_dir(a),
   invalid: a => typeof a === 'undefined' || a === null,
-  empty: a => typeof a === 'undefined' || a === null || a === '',
-  contain: (a, b) => a.indexOf(b) > -1
+  contain: (a, b) => {
+    if ((is.array(a) || is.string(a)) && a.indexOf(b) > -1) {
+      return true;
+    }
+    if (is.object(a) && !is.invalid(a[b])) {
+      return true;
+    }
+    return false;
+  },
+  empty: a => {
+    if (typeof a === 'undefined' || a === null || a === '') {
+      return true;
+    }
+    if (is.object(a) && Object.keys(a).length === 0) {
+      return true;
+    }
+    if (is.array(a) && a.length === 0) {
+      return true;
+    }
+    return false;
+  }
 };
 
 module.exports = is;
