72e64da50aab66ed23f9fcb09f81c9ad313bafaa
axios <axiosleo@foxmail.com>
2021-02-14 13:56:54 +0800

    Bump 1.2.3

--- bin/cli-tool.js
+++ bin/cli-tool.js
@@ -6,7 +6,7 @@ const path = require('path');
 const { App } = require('../main');
 const app = new App({
   name: 'cli',
-  version: '1.2.2',
+  version: '1.2.3',
   desc: 'application_desc',
   commands_dir: path.join(__dirname, '../commands'),
   commands_sort: ['init', 'make']
