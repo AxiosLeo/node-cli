9dca3b60206e5985005c3efc4796a9b6e8e7109f
axios <axiosleo@foxmail.com>
2021-02-15 21:26:05 +0800

    chore: patch bin code

--- bin/cli-tool.js
+++ bin/cli-tool.js
@@ -4,15 +4,22 @@
 
 const path = require('path');
 const { App } = require('../main');
+
+// init app
 const app = new App({
   name: 'cli',
   version: '1.2.5',
   desc: 'application_desc',
   commands_dir: path.join(__dirname, '../commands'),
-  commands_sort: ['init', 'make']
+  commands_sort: ['init', 'make'],
+  locale: {
+    dir: path.join(__dirname, '../locales'),
+    sets: ['en-US', 'zh-CN']
+  }
 });
-app.locale({
-  dir: path.join(__dirname, '../locales'),
-  sets: ['en-US', 'zh-CN']
-});
+
+// enable use locales
+app.locale();
+
+// start cli application
 app.start();
