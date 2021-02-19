fedaaf46039089c31f23faae8f2e10e5ff1d386f
axios <axiosleo@foxmail.com>
2021-02-20 00:50:16 +0800

    Bump 1.2.10

--- bin/cli-tool.js
+++ bin/cli-tool.js
@@ -8,7 +8,7 @@ const { App } = require('../main');
 // init app
 const app = new App({
   name: 'cli',
-  version: '1.2.9',
+  version: '1.2.10',
   desc: 'application_desc',
   commands_dir: path.join(__dirname, '../commands'),
   commands_sort: ['init', 'make'],
