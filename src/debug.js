ea1c3b623da38e591c5f8147d014063e02e1e48d
axios <axiosleo@foxmail.com>
2021-07-04 10:58:40 +0800

    feat: show trace string when call dump/halt/jump

--- src/debug.js
+++ src/debug.js
@@ -5,27 +5,34 @@ const { prompt } = require('enquirer');
 
 var count = 0;
 
+function trace(label) {
+  const stack = (new Error()).stack;
+  let tmp = stack.split(os.EOL);
+  let local = tmp[3].indexOf('at Object.jump') > -1 ? tmp[4] : tmp[3];
+  process.stdout.write(`\x1b[33m${label} ${local.trim()}\x1b[0m${os.EOL}`);
+}
+
 function dump(...data) {
   if (!data || !data.length) {
     return;
   }
+  trace('dump');
   // eslint-disable-next-line no-console
   data.forEach(d => console.log(d));
 }
 
 function halt(...data) {
-  const stack = (new Error()).stack;
-  let tmp = stack.split(os.EOL);
-  let local = tmp[2].indexOf('at Object.jump') > -1 ? tmp[3] : tmp[2];
-  process.stdout.write(`\x1b[33mhalt ${local.trim()}\x1b[0m${os.EOL}`);
-  dump(...data);
+  trace('halt');
+  data.forEach(d => console.log(d));
   process.exit(-1);
 }
 
 function jump(jumpNumber = 0, ...data) {
+  trace('jump');
   if (count === jumpNumber) {
     count = 0;
-    halt(...data);
+    data.forEach(d => console.log(d));
+    process.exit(-1);
   } else {
     count++;
   }
@@ -38,7 +45,7 @@ function stack(...data) {
     msg = data[0];
     data = data.slice(1);
   }
-  dump(...data);
+  data.forEach(d => console.log(d));
   throw new Error(msg);
 }
 
@@ -48,14 +55,14 @@ function warning(...data) {
     msg = data[0];
     data = data.slice(1);
   }
-  dump(...data);
+  data.forEach(d => console.log(d));
   if (msg.length) {
     process.stdout.write(`\x1b[33m${os.EOL}[WARNING] ${msg}\x1b[0m${os.EOL}`);
   }
 }
 
 async function pause(...data) {
-  dump(...data);
+  data.forEach(d => console.log(d));
   return new Promise((resolve, reject) => {
     prompt([
       {
@@ -75,11 +82,11 @@ function error(...data) {
     msg = data[0];
     data = data.slice(1);
   }
-  dump(...data);
+  data.forEach(d => console.log(d));
   if (msg.length) {
     process.stdout.write(`\x1b[31m${os.EOL}[ERROR] ${msg}\x1b[0m${os.EOL}${os.EOL}`);
   }
-  halt();
+  process.exit(-1);
 }
 
 module.exports = {
