df5efb08c0d80a19068890e1d692b0d9c1d9811e
axios <axiosleo@foxmail.com>
2021-09-08 17:30:58 +0800

    chore: patch cmd sync_foreach test case

--- tests/cmd.tests.js
+++ tests/cmd.tests.js
@@ -8,18 +8,10 @@ const {
 } = require('../src/helper/cmd');
 
 describe('cmd test case', function () {
-  it('sync promise all', async function () {
+  it('sync traverse proimse method for array', async function () {
+    let data = [1, 2, 3, 4, 5];
     let count = 0;
-    let data = { a: 0, b: 1, c: 2, d: 3, e: 4 };
-    await _sync_foreach(data, async function (value, key) {
-      await _sleep(Math.floor(Math.random() * 300));
-      // to prove that the async functions are executed sequentially
-      expect(count).to.be.equal(value);
-      count++;
-    });
 
-    data = [1, 2, 3, 4, 5];
-    count = 0;
     await _sync_foreach(data, async function (item, index) {
       await _sleep(Math.floor(Math.random() * 300));
       // to prove that the async functions are executed sequentially
@@ -28,8 +20,19 @@ describe('cmd test case', function () {
     });
   });
 
-  it('not throw error when sync empty array or object',async function () {
-    let data = { };
+  it('sync traverse proimse method for object', async function () {
+    let count = 0;
+    let data = { a: 0, b: 1, c: 2, d: 3, e: 4 };
+    await _sync_foreach(data, async function (value, key) {
+      await _sleep(Math.floor(Math.random() * 300));
+      // to prove that the async functions are executed sequentially
+      expect(count).to.be.equal(value);
+      count++;
+    });
+  });
+
+  it('not throw error when sync empty array or object', async function () {
+    let data = {};
     await _sync_foreach(data);
 
     data = [];
