50c1eb795d6dac527ad30fe504b22a70a2d2946e
axios <axiosleo@foxmail.com>
2021-09-22 11:27:05 +0800

    feat: get current git branch name

--- src/helper/git.js
+++ src/helper/git.js
@@ -1,13 +1,23 @@
 'use strict';
 
+const { _shell } = require('./cmd');
 const { _find_root } = require('./fs');
 
 const find_git_root = async (cwd = null) => {
   return await _find_root('.git', cwd, 'Please executed in the git directory path.');
 };
 
+const curr_branch_name = async (cwd = null) => {
+  cwd = await find_git_root(cwd);
+  const res = await _shell('git branch |grep "*"', cwd, false);
+  return res.stdout.substr(2).split('\n').join('');
+};
+
 module.exports = {
   path: {
     root: find_git_root
+  },
+  branch: {
+    curr: curr_branch_name,
   },
 };
--- tests/git.tests.js
+++ tests/git.tests.js
@@ -5,9 +5,20 @@ const chai = require('chai');
 const expect = chai.expect;
 chai.use(require('chai-as-promised'));
 
+const { _shell } = require('../src/helper/cmd');
+const { _remove, _copy } = require('../src/helper/fs');
 const git = require('../src/helper/git');
+const basepath = path.join(__dirname, '../../node-cli-tmp-project');
 
 describe('git test case', function () {
+  this.beforeAll(async () => {
+    await _copy(path.join(__dirname, '../'), basepath, true);
+  });
+
+  this.afterAll(async () => {
+    await _remove(basepath);
+  });
+
   describe('find git root', function () {
     it('throw an error If not executed in the git path', function () {
       expect(git.path.root(path.join(__dirname, '../../'))).to.be.rejectedWith('Please executed in the git directory path.');
@@ -18,6 +29,18 @@ describe('git test case', function () {
       expect(await git.path.root(path.join(__dirname, '../src/helper/fs.js'))).to.be.equal(gitpath);
 
       expect(await git.path.root()).to.be.equal(gitpath);
+    });
+  });
+
+  describe('get current git branch name', function () {
+    it('should be ok to get current branch name', async function () {
+      expect(!!(await git.branch.curr())).to.be.equal(true);
+      const first = await git.branch.curr(basepath);
+      expect(!!first.length).to.be.equal(true);
+      await _shell('git checkout -b test-tmp-branch', basepath);
+      expect(await git.branch.curr(basepath)).to.be.equal('test-tmp-branch');
+      await _shell(`git checkout ${first}`, basepath);
+      await _shell('git branch -D test-tmp-branch', basepath);
     });
   });
 });
