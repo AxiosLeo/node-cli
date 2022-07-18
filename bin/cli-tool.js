a36405cef7f4e8f51658a0c00a8cc3689428e342
axiosleo <axiosleo@foxmail.com>
2022-07-19 07:08:28 +0800

    Bump 1.4.10

--- bin/cli-tool.js
+++ bin/cli-tool.js
@@ -8,7 +8,7 @@ const { App } = require('../main');
 // init app
 const app = new App({
   name: 'cli-tool',
-  version: '1.4.9',
+  version: '1.4.10',
   desc: 'application_desc',
   commands_dir: path.join(__dirname, '../commands'),
   commands_sort: ['help', 'init', 'make', 'exec'],
