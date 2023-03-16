ad9b86ab1c7e95244eb34d7d9f7bcee12491d05d
axiosleo <axiosleo@foxmail.com>
2023-03-16 17:05:44 +0800

    ci: fix

---
+++ src/lib/exec.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// Mini wrapper around `child_process` to make it behave a little like `execa`.
+
+const promisify = require('util').promisify;
+const childProcess = require('child_process');
+
+const execFile = promisify(childProcess.execFile);
+
+/**
+@param {string} command
+@param {string[]} arguments_
+@returns {Promise<import('child_process').ChildProcess>}
+*/
+async function exec(command, arguments_) {
+  const subprocess = await execFile(command, arguments_, { encoding: 'utf8' });
+  subprocess.stdout = subprocess.stdout.trim();
+  return subprocess;
+}
+
+/**
+@param {string} command
+@param {string[]} arguments_
+@returns {string}
+*/
+function execSync(command, arguments_) {
+  return childProcess.execFileSync(command, arguments_, {
+    encoding: 'utf8',
+    stdio: ['ignore', 'pipe', 'ignore'],
+  }).trim();
+}
+
+module.exports = {
+  exec,
+  execSync
+};
