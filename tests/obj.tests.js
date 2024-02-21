6220bce3fab50801daa51f451bee60a81891e7f8
axiosleo <axiosleo@foxmail.com>
2024-02-21 11:54:09 +0800

    feat: Add obj._array2tree and obj._tree2array helper methods

--- tests/obj.tests.js
+++ tests/obj.tests.js
@@ -2,7 +2,7 @@
 
 const expect = require('chai').expect;
 const { Configuration } = require('..');
-const { _deep_clone, _flatten, _unflatten } = require('../src/helper/obj');
+const { _deep_clone, _flatten, _unflatten, _array2tree, _tree2array } = require('../src/helper/obj');
 
 describe('obj test case', function () {
   it('flatten', function () {
@@ -65,5 +65,69 @@ describe('obj test case', function () {
     clone.a.b.c.d[0].x = '3';
     expect(complexObj.a.b.c.d.length).to.be.equal(1);
     expect(complexObj.a.b.c.d[0].x).to.be.equal('1');
+  });
+  it('array2tree', function () {
+    const data = [
+      { id: 1, parent_id: 0 },
+      { id: 2, parent_id: 3 },
+      { id: 3, parent_id: 1 },
+      { id: 4, parent_id: 2 },
+      { id: 5, parent_id: 6 },
+      { id: 6, parent_id: 7 },
+      { id: 7, parent_id: 5 }
+    ];
+    const res = _array2tree(data, { parent_index: 'parent_id', data_index: 'id', child_name: 'child' });
+    expect(res.length).to.be.equal(1);
+    expect(res[0].id).to.be.equal(1);
+    expect(res[0].child.length).to.be.equal(1);
+    expect(res[0].child[0].id).to.be.equal(3);
+    expect(res[0].child[0].child.length).to.be.equal(1);
+    expect(res[0].child[0].child[0].id).to.be.equal(2);
+    expect(res[0].child[0].child[0].child.length).to.be.equal(1);
+    expect(res[0].child[0].child[0].child[0].id).to.be.equal(4);
+    expect(res[0].child[0].child[0].child[0].child.length).to.be.equal(0);
+    expect(res[0].child[0].child[0].child[0].parent_id).to.be.equal(2);
+
+    // throw error when data is not array
+    expect(() => _array2tree(null)).to.throw('data must be an array');
+    expect(() => _array2tree([{ id: 1 }])).to.throw('data must have "parent_id" and "id"');
+    expect(() => _array2tree([{ id: 1, parent_id: 0, child: [] }])).to.throw('child name "child" is reserved for child data, please use another name');
+  });
+
+  it('tree2array', function () {
+    const data = [
+      {
+        'id': 1,
+        'parent_id': 0,
+        'child': [
+          {
+            'id': 3,
+            'parent_id': 1,
+            'child': [
+              {
+                'id': 2,
+                'parent_id': 3,
+                'child': [
+                  {
+                    'id': 4,
+                    'parent_id': 2,
+                    'child': []
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ];
+    let res = _tree2array(data, { parent_index: 'parent_id', data_index: 'id', child_name: 'child' });
+    expect(JSON.stringify(_array2tree(res))).to.equal(JSON.stringify(data));
+
+    const data2 = _deep_clone(data);
+    delete data2[0].parent_id;
+    delete data2[0].child[0].parent_id;
+    delete data2[0].child[0].child[0].parent_id;
+    res = _tree2array(data2[0]);
+    expect(JSON.stringify(_array2tree(res))).to.equal(JSON.stringify(data));
   });
 });
