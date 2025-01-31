732c09d1fa7283fa9115e348f5242e568b37291e
axiosleo <axiosleo@foxmail.com>
2025-01-31 19:27:10 +0800

    Bump 1.6.6

--- commands/init.js
+++ commands/init.js
@@ -34,7 +34,7 @@ class InitCommand extends Command {
       },
       'license': 'MIT',
       'dependencies': {
-        '@axiosleo/cli-tool': '^1.6.5'
+        '@axiosleo/cli-tool': '^1.6.6'
       },
       'devDependencies': {
         'eslint': '*'
